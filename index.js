3b000014f044f1af2865bb5ac4583d8d5157c85e
bskbala <bskbala410@gmail.com>
2021-02-18 23:45:31 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+  // ways to print  in JavaScript
+
+  // JavaScript Console API
+  //   console.log("Hello world")
+  //   console.warn("This is warning")
+  //   console.erroe("This is Error")
+
+  //   // alert("Its mee")
+  //   document.write("This is my Document")
+
+  //   Java Script Variables
+
+  // var x, y;// Declaring Variables
+  // x = "Praveen ";// Defening Variabels
+  // console.log(typeof (x));
+  // x = 900;
+  // console.log(typeof (x));
+  // x = true;
+  // console.log(typeof (x));
+
+
+  // var num1 =34;
+  // var num2 =96;
+  // console.log(num1 + num2);
+
+  // // Data Types in Java Script
+
+  // var num1 = 455;
+  // var num2 = 666;
+
+  // var str1 ="This  is a string";
+  // var str2 = "this is alos a string";
+
+  // // Objects
+  // var marks = {
+  //      ravi:35,
+  //      shubham:78,
+  //      harry:99.997
+  // }
+  // console.log(marks);
